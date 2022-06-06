7971ba9b73c779a7ed1b7f2a2cf6b80b9ce955b6
galo36 <99937579+galo36@users.noreply.github.com>
2022-06-06 01:47:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -227,10 +227,10 @@ def onmessage(update,bot:ObigramClient):
     try:
         thread = bot.this_thread
         username = update.message.sender.username
-        tl_admin_user = os.environ.get('tl_admin_user')
+        tl_admin_user = os.environ.get('DAMIANBZM')
 
         #set in debug
-        tl_admin_user = 'Yama_Tsukami'
+        tl_admin_user = 'DAMIANBZM'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -251,7 +251,7 @@ def onmessage(update,bot:ObigramClient):
             mensaje = "❌No tienes Acceso❌.\nPor favor Contacta con mi Programador @Yama_Tsukami\n"
             intento_msg = "💢El usuario @"+username+ " ha intentando usar el bot sin permiso💢"
             bot.sendMessage(update.message.chat.id,mensaje)
-            bot.sendMessage(5061352415,intento_msg)
+            bot.sendMessage(2040050486,intento_msg)
             return
 
 
@@ -615,9 +615,9 @@ def onmessage(update,bot:ObigramClient):
             #    bot_token = os.environ.get('bot_token')
             #    
                 # set in debug
-            #    api_id = 18693993
-            #    api_hash = '382ee6b53bdd0df66a52ea9779c62424'
-            #    bot_token = '5371733981:AAF-C9H4xrVMqHYgFdFHXxqyVFkvY99Sdrw'
+            #    api_id = 16923599
+            #    api_hash = 'f69ab9f221ce44a4f0541e6f4624f431'
+            #    bot_token = '5312605097:AAElR2O-3wykmjPgChgpkXTgmAQDlQVE0J4'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -633,7 +633,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5347610697:AAEHt62no3ioub7ghin5SWZae5qi6LpAfxk'
+    bot_token = '5312605097:AAElR2O-3wykmjPgChgpkXTgmAQDlQVE0J4'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
